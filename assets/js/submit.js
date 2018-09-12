4a7ee03b115a58371985d1300e019a72904f8980
brute4s99 [LAPTOP] <piyushaggarwal002@gmail.com>
2018-09-13 00:57:53 +0530

    Message sending works now! yay !!🎉

---
+++ assets/js/submit.js
@@ -0,0 +1,36 @@
+// magic.js
+$(document).ready(function() {
+
+    // process the form
+    $('form').submit(function(event) {
+
+        // get the form data
+        // there are many ways to get this data using jQuery (you can use the class or id also)
+        var formData = {
+            'name'              : $('input[name=name]').val(),
+            'email'             : $('input[name=email]').val(),
+            'message'    : $('input[name=message]').val()
+        };
+
+        // process the form
+        $.ajax({
+            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+            url         : 'https://script.google.com/macros/s/AKfycby_hOCPRAwMDXfct2NFD15ifNYpl-2ZFZh6lLsk/exec', // the url where we want to POST
+            data        : formData, // our data object
+            dataType    : 'json', // what type of data do we expect back from the server
+            encode      : true
+        })
+            // using the done promise callback
+            .done(function(data) {
+
+                // log data to the console so we can see
+                console.log(data); 
+                alert("We have received your message. We will contact you within 2 working days.")
+                // here we will handle errors and validation messages
+            });
+
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
+    });
+
+});
